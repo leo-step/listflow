9cb4769814b9333a34a1ca5da67b6f60797247e4
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-11-28 12:45:43 -0500

    tagging progress

--- src/inference/tagging.ts
+++ src/inference/tagging.ts
@@ -1,19 +1,10 @@
 import { SMTPEmail } from "../types/mailTypes";
-
-const RULE_BASED_TAGS = [
-  "Free Food",
-  "All Undergrad",
-  "WhitmanWire",
-  "MatheyMail",
-  "Pace Center",
-  "CampusRec",
-  "TigerAlert",
-  "Google Slides",
-];
+import { getOpenAIJsonResponse, userPrompt } from "../utils/openai";
 
 const TOPIC_TAGS = [
   "Speaker Event",
   "Internship",
+  "Eating Club",
   "Tower Club",
   "Cap and Gown Club",
   "Boba",
@@ -25,16 +16,65 @@ const TOPIC_TAGS = [
   "Sale",
 ];
 
+const TAG_BATCH_SIZE = 32;
+
+type TagsDict = {
+  [key: string]: boolean;
+};
+
 const getRuleBasedTags = (data: SMTPEmail) => {
-  return [RULE_BASED_TAGS[0]];
+  const ruleTags: string[] = [];
+  const toAddresses = data.envelopeTo.map((value) => value.address);
+
+  // const RULE_BASED_TAGS = [
+  //   "Free Food",
+  //   "All Undergrad",
+  //   "WhitmanWire",
+  //   "MatheyMail",
+  //   "Pace Center",
+  //   "CampusRec",
+  //   "TigerAlert",
+  //   "Google Slides",
+  // ];
+
+  if (toAddresses.includes("freefood@princeton.edu")) {
+    ruleTags.push("Free Food");
+  }
+
+  return ruleTags;
 };
 
 const getTopicTags = async (data: SMTPEmail) => {
-  return [TOPIC_TAGS[0]];
+  const classifyTagsPrompt = userPrompt((tags: string[]) => ``); // TODO: needs prompt
+
+  const promises: Promise<TagsDict | null>[] = [];
+  for (let i = 0; i < TOPIC_TAGS.length; i += TAG_BATCH_SIZE) {
+    const topicTagsBatch = TOPIC_TAGS.slice(i, i + TAG_BATCH_SIZE);
+    promises.push(
+      getOpenAIJsonResponse<TagsDict>([classifyTagsPrompt(topicTagsBatch)])
+    );
+  }
+
+  const results = await Promise.all(promises);
+  // if (results.includes(null)) {
+  //   throw new Error("null result from OpenAI JSON response in getTopicTags");
+  // }
+
+  const tags = results
+    .filter((result: TagsDict | null) => result !== null)
+    .map((result: TagsDict) => getTagsWithTrue(result));
+
+  return tags.reduce((a: string[], b: string[]) => [...a, ...b]);
+};
+
+const getTagsWithTrue = (tagsDict: TagsDict) => {
+  return Object.entries(tagsDict)
+    .filter(([key, value]) => value === true)
+    .map(([key]) => key);
 };
 
 export const getTags = async (data: SMTPEmail) => {
   const ruleTags = getRuleBasedTags(data);
   const topicTags = await getTopicTags(data);
-  return [...ruleTags, ...topicTags];
+  return Array.from(new Set([...ruleTags, ...topicTags]));
 };
--- src/models/emailModel.ts
+++ src/models/emailModel.ts
@@ -6,7 +6,7 @@ const EmailSchema = new Schema({
   subject: { type: String, required: true },
   time: { type: Number, required: true }, // unix
   messageId: { type: String, required: true },
-  from: { type: String, required: true },
+  from: { type: String, required: true }, // should include TO as well
   computed: {
     expiry: { type: Number, required: true },
     embedding: { type: [Number], required: true },
