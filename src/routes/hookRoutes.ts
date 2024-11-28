20b722f801472531896e9ac4cc66544dd277bfee
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-11-28 16:16:37 -0500

    comment

--- src/routes/hookRoutes.ts
+++ src/routes/hookRoutes.ts
@@ -10,5 +10,7 @@ const router = Router();
 router.post("/create", createHook);
 router.post("/update", updateHook);
 router.post("/delete", deleteHook);
+// should have test endpoint where you give it whatever email content you want
+// and then it calls your hook by id right away
 
 export default router;
