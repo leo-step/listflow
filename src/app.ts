a69e532cfb26300694016d378c6fca56e65ca445
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-11-28 10:59:36 -0500

    mail controller methods

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import routes from "./routes";
-import { handleMessage } from "./controllers/mailController";
+import {
+  //   handleAuthorizeUser,
+  handleError,
+  handleMessage,
+  handleStartMessage,
+  handleValidateRecipient,
+  handleValidateSender,
+} from "./controllers/mailController";
 import authMiddleware from "./middlewares/authMiddleware";
 
 const app = express();
@@ -11,6 +18,11 @@ app.use(authMiddleware);
 
 app.use("/api", routes);
 
+// nodeMailin.on("authorizeUser", handleAuthorizeUser);
+nodeMailin.on("validateSender", handleValidateSender);
+nodeMailin.on("validateRecipient", handleValidateRecipient);
+nodeMailin.on("startMessage", handleStartMessage);
 nodeMailin.on("message", handleMessage);
+nodeMailin.on("error", handleError);
 
 export { app, nodeMailin };
--- src/controllers/mailController.ts
+++ src/controllers/mailController.ts
@@ -5,6 +5,63 @@ import { getExpiryTime } from "../inference/expiry";
 import { getEmailEmbedding } from "../inference/embedding";
 import { getTags } from "../inference/tagging";
 
+const AUTHORIZED_SENDERS: string[] = ["sender@example.com"];
+const AUTHORIZED_RECIPIENTS: string[] = [];
+
+class ErrorWithResponseCode extends Error {
+  responseCode: number;
+
+  constructor(message: string, responseCode: number) {
+    super(message);
+    this.responseCode = responseCode;
+
+    Object.setPrototypeOf(this, ErrorWithResponseCode.prototype);
+  }
+}
+
+// nodeMailin.on("authorizeUser", () => {});
+// export const handleAuthorizeUser = async (
+//   connection: SMTPConnection,
+//   username: string,
+//   password: string,
+//   done: (error: Error, authorized: boolean) => void
+// ) => {};
+
+// nodeMailin.on("validateSender", () => {});
+export const handleValidateSender = async (
+  session: any,
+  address: string,
+  callback: (error?: Error) => void
+) => {
+  if (AUTHORIZED_SENDERS.length == 0 || AUTHORIZED_SENDERS.includes(address)) {
+    callback();
+  } else {
+    const err = new ErrorWithResponseCode("Failed validate sender", 530);
+    callback(err);
+  }
+};
+
+// nodeMailin.on("validateRecipient", () => {});
+export const handleValidateRecipient = async (
+  session: any,
+  address: string,
+  callback: (error?: Error) => void
+) => {
+  if (
+    AUTHORIZED_RECIPIENTS.length == 0 ||
+    AUTHORIZED_RECIPIENTS.includes(address)
+  ) {
+    callback();
+  } else {
+    const err = new ErrorWithResponseCode("Failed validate recipient", 550);
+    callback(err);
+  }
+};
+
+export const handleStartMessage = (connection: SMTPConnection) => {
+  console.log(connection);
+};
+
 export const handleMessage = async (
   connection: SMTPConnection,
   data: SMTPEmail,
@@ -42,4 +99,9 @@ export const handleMessage = async (
   // if (connection.remoteAddress !== "127.0.0.1") {
 
   // }
+};
+
+// nodeMailin.on("error", () => {});
+export const handleError = (error: Error) => {
+  console.log(error);
 };
