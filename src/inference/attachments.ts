31b3f0d2ac8fbbe35aa5f504a8564c71aa568638
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-12-24 13:04:41 -0500

    use google gemini for images

--- src/inference/attachments.ts
+++ src/inference/attachments.ts
@@ -1,6 +1,6 @@
 import { ImageCacheModel } from "../models/imageCacheModel";
 import { Attachment, SMTPEmail } from "../types/mailTypes";
-import { openai } from "../utils/openai";
+import { getImageDescription } from "../utils/googleai";
 
 const MAX_ATTACHMENTS = 10;
 
@@ -27,35 +27,8 @@ const processImageAttachment = async (image: Attachment) => {
     return "Image attachment was too big to process.";
   }
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-4o",
-    messages: [
-      {
-        role: "user",
-        content: [
-          {
-            type: "text",
-            text: "Describe what is in this image. If there is any text, make sure to transcribe it word for word.",
-          },
-          {
-            type: "image_url",
-            image_url: {
-              // TODO: this image should be stored and be accessible through S3, store link in image cache
-              url: `data:${image.contentType};base64,${image.content.toString(
-                "base64"
-              )}`,
-            },
-          },
-        ],
-      },
-    ],
-  });
-
-  const description = response.choices[0].message.content;
-
-  if (!description) {
-    return "Image description could not be processed.";
-  }
+  // TODO: this image should be stored and be accessible through S3, store link in image cache
+  const description = await getImageDescription(image);
 
   const imageCache = new ImageCacheModel({
     checksum: image.checksum,
@@ -65,16 +38,3 @@ const processImageAttachment = async (image: Attachment) => {
 
   return description;
 };
-
-// attachments: [
-//     {
-//       type: 'attachment',
-//       content: <Buffer ff d8 ff e1 2d 89 45 78 69 66 00 00 4d 4d 00 2a 00
-//       release: null,
-//       contentDisposition: 'inline',
-//       filename: 'image0.jpeg',
-//       headers: [Map],
-//       checksum: '6e00c7e2a852e56397b3f97f23e2f09d',
-//       size: 3490919
-//     }
-//   ],
---
+++ src/utils/googleai.ts
@@ -0,0 +1,29 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { Attachment } from "../types/mailTypes";
+
+// needed because node 16
+const fetch = require("node-fetch");
+
+global.fetch = fetch;
+global.Headers = fetch.Headers;
+global.Request = fetch.Request;
+global.Response = fetch.Response;
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
+
+export async function getImageDescription(
+  image: Attachment,
+  modelName: string = "gemini-1.5-flash"
+) {
+  const model = genAI.getGenerativeModel({ model: `models/${modelName}` });
+  const result = await model.generateContent([
+    {
+      inlineData: {
+        data: image.content.toString("base64"),
+        mimeType: image.contentType,
+      },
+    },
+    "Describe what is in this image. If there is any text, make sure to transcribe it word for word.",
+  ]);
+  return result.response.text();
+}
