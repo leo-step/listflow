6e88b172d85fb072dc49078c1bd1dca88de60582
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-12-28 11:28:34 -0500

    fix issue

--- deployment/user_data.sh
+++ deployment/user_data.sh
@@ -6,10 +6,10 @@ cd listflow
 export MONGO_URI="${MONGO_URI}"
 export OPENAI_API_KEY="${OPENAI_API_KEY}"
 export GEMINI_API_KEY="${GEMINI_API_KEY}"
-export AWS_ACCESS_KEY_ID = "${AWS_ACCESS_KEY_ID}"
-export AWS_SECRET_ACCESS_KEY = "${AWS_SECRET_ACCESS_KEY}"
-export BUCKET_NAME = "${BUCKET_NAME}"
-export BUCKET_REGION = "${BUCKET_REGION}"
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+export BUCKET_NAME="${BUCKET_NAME}"
+export BUCKET_REGION="${BUCKET_REGION}"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
