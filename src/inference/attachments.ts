f4c216203bff8541f5fcd2880ef5be9e05886bac
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-12-28 11:17:56 -0500

    try it the old way

--- src/inference/attachments.ts
+++ src/inference/attachments.ts
@@ -1,11 +1,24 @@
 import { ImageModel } from "../models/imageModel";
 import { Attachment, SMTPEmail } from "../types/mailTypes";
 import { getImageDescription } from "../utils/googleai";
-import { s3Client, bucketName } from "../s3";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const bucketName = process.env.BUCKET_NAME || "";
+const bucketRegion = process.env.BUCKET_REGION || "";
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID || "";
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || "";
+
+const s3Client = new S3Client({
+  region: bucketRegion,
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+});
 
 const MAX_ATTACHMENTS = 10;
 
